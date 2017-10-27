9b5bbc2d376e2859bb44bc45bbfc26623b89673c
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-27 22:45:07 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -10,9 +10,7 @@ class RepositoriesController < ApplicationController
         req.params['client_id'] = 'Iv1.b0e1fe1a1a5f8003'
         req.params['client_secret'] = '08bf9fb085f4a30c059aa06783b9445315815651'
         req.params['q'] = params[:query]
-
       end
-
       body = JSON.parse(@resp.body)
       if @resp.success?
         @repos = body['items']
@@ -20,7 +18,7 @@ class RepositoriesController < ApplicationController
         @error = body['meta']['errorDetail']
       end
       render 'search'
-
     end
   end
+  
 end
