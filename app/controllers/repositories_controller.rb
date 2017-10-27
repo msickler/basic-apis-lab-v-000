3bf08347cf329861ab36d906c6ec1fdc0b4bd4b5
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-27 22:36:56 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -6,7 +6,7 @@ class RepositoriesController < ApplicationController
 
   def github_search
     begin
-      @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+      @resp = Faraday.get 'https://api.github.com/search/repositories?access_token=&q=#{params['query']}' do |req|
         req.params['client_id'] = 'Iv1.b0e1fe1a1a5f8003'
         req.params['client_secret'] = '08bf9fb085f4a30c059aa06783b9445315815651'
         req.params['v'] = '20160201'
